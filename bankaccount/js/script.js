6725910f0931b929f904996c621374ec9ca0e4f0
popovevg <popovevg2013@gmail.com>
2020-05-01 11:52:19 -0700

    last update

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -1,23 +1,51 @@
-/*eslint-env browser*/
-window.addEventListener('load', ()=> {
-    let userName;
-    let userDeposit;
-    let userWithdrow;
-    let outPut = document.getElementById('output');
+var bankAccount = function(ownerName) {
+    let balance = 0;
+    let owner = ownerName;
+    return {
+        getBalance: function() {
+            return balance;
+        },
+        getOwnerName: function() {
+            return owner;
+        },
+        withdrawal: function(withdrawalAmount) {
+            if (isNaN(parseInt(withdrawalAmount))) {
+                window.alert("Enter correct withdrow amount!");
+            }
+            else {
+                balance = parseInt(this.getBalance()) - parseInt(withdrawalAmount);
+                $('userBalanceText').textContent = balance;
+            }
+        },
+        deposit: function(depositAmount) {
+            if (isNaN(parseInt(depositAmount))) {
+                window.alert("Enter correct deposit amount!");
+            }
+            else {
+                balance = parseInt(this.getBalance()) + parseInt(depositAmount);
+                $('userBalanceText').textContent = balance;
+            }
+        }
+    }
+}
 
-    let nameButton = document.getElementById('name');
-    nameButton.addEventListener('click', ()=>{
-        userName = window.prompt('Enter your name:');
-    });
+var $ = function (id) {
+    "use strict";
+    return window.document.getElementById(id);
+};
 
-    let depositButton = document.getElementById('deposit');
-    depositButton.addEventListener('click', ()=>{
-        userDeposit = window.prompt('Enter your deposit amount:');
-    });
+var bank = bankAccount(prompt('Enter user Name to start using the Bank App:'));
 
-    let withdrowButton = document.getElementById('withdrow');
-    withdrowButton.addEventListener('click', ()=>{
-        userWithdrow = window.prompt('Enter your withdrow amount:');
-    });
-})
-
+window.addEventListener('load', ()=>{
+    "use strict";
+    $('userNameText').textContent = bank.getOwnerName();
+    $('name').addEventListener('click', function() {
+        $('userNameText').textContent = window.prompt('Enter your name:');
+    })
+    $('deposit').addEventListener('click', function() {
+        bank.deposit(window.prompt('Enter your deposit amount:'))
+    }) ;
+    $('withdrow').addEventListener('click', function() {
+        bank.withdrawal(window.prompt('Enter your withdrow amount:'))
+    }) ;
+});
